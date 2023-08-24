f043625f791a4fc7a2b4e3ec7dd774b83d202559
Tamias <n.tamia5@icloud.com>
2023-08-24 21:59:30 +0300

    update task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,19 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListEl = document.querySelector('.gallery');
+
+const galleryItemsCardList = galleryItems
+    .map(({ preview, original, description }) =>
+        `<li class='gallery__item'>
+        <a class='gallery__link' href='${original}'>
+            <img class='gallery__image' src='${preview}' alt='${description}'>
+        </a>
+    </li>`)
+    .join('');
+
+galleryListEl.insertAdjacentHTML('beforeend', galleryItemsCardList);
+new SimpleLightbox('.gallery a', {captionsData: 'alt', captionsDelay: 250});
