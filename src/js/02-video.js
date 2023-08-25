a1c28d9d616a5f79b33735d30efb256606877cf3
Tamias <n.tamia5@icloud.com>
2023-08-25 15:57:50 +0300

    update 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const playerEl = document.getElementById('vimeo-player');
+const player = new Player(playerEl);
+
+player.on('timeupdate', throttle(playerTime, 1000));
+
+function playerTime(data) {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+}
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
